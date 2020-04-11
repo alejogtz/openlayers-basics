c9da759251c9c2025acbbb2075811f17a9d13d86
Alejo Gutierrez <alejox.gutierrez@gmail.com>
2020-04-10 20:01:15 -0500

    last commit was overwriten and this commit achieve implement measure functions

--- src/app/ol/components/map/map.component.ts
+++ src/app/ol/components/map/map.component.ts
@@ -1,45 +1,24 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import Map from 'ol/Map';
-import View, { FitOptions } from 'ol/View';
-
-// Layers
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { transform } from 'ol/proj';
+import { View, Map, Feature, Overlay, MapBrowserEvent } from 'ol';
 import Layer from 'ol/layer/Layer';
 import TileLayer from 'ol/layer/Tile';
-import VectorImage from 'ol/layer/VectorImage';
-import LayerGroup from 'ol/layer/Group';
-import ImageLayer from 'ol/layer/Image';
-
-
-// Sources
-import OSM from 'ol/source/OSM';
-import Stamen from 'ol/source/Stamen';
-import TileWMS from 'ol/source/TileWMS';
-import Vector from 'ol/source/Vector';
-import ImageWMS from 'ol/source/ImageWMS';
-
-
-// Projections
-import { transform, transformExtent } from 'ol/proj';
-
-// Formats
-import GeoJSON from 'ol/format/GeoJSON';
-
-// Styles
 import Style from 'ol/style/Style';
-import Stroke from 'ol/style/Stroke';
 import Fill from 'ol/style/Fill';
-
-// Others
-import { createXYZ } from 'ol/tilegrid';
-import { Pixel } from 'ol/pixel';
+import Stroke from 'ol/style/Stroke';
 import VectorLayer from 'ol/layer/Vector';
-import Geometry from 'ol/geom/Geometry';
-import { MapBrowserEvent, Feature } from 'ol';
-import Point from 'ol/geom/Point';
-import { Extent, createEmpty, extend } from 'ol/extent';
+import OSM from 'ol/source/OSM';
 import VectorSource from 'ol/source/Vector';
-import { mapToMapExpression } from '@angular/compiler/src/render3/util';
-import { Size } from 'ol/size';
+import Icon from 'ol/style/Icon';
+import Geometry from 'ol/geom/Geometry';
+import Polygon from 'ol/geom/Polygon';
+import LineString from 'ol/geom/LineString';
+import Draw, { DrawEvent } from 'ol/interaction/Draw';
+import OverlayPositioning from 'ol/OverlayPositioning';
+import { ListenerFunction, EventsKey } from 'ol/events';
+import { unByKey } from 'ol/Observable';
+import BaseEvent from 'ol/events/Event';
+import GeometryType from 'ol/geom/GeometryType';
 
 @Component({
   selector: 'app-map',
@@ -49,162 +28,261 @@ import { Size } from 'ol/size';
 export class MapComponent implements AfterViewInit {
 
   @ViewChild('map') divMap: ElementRef;
-
+  @ViewChild('image') imageContainer: ElementRef;
+  @ViewChild('type') selectElement: ElementRef;
   map: Map;
+
+  // tslint:disable-next-line: member-ordering
+  sketch: Feature;
+  helpTooltipElement: HTMLElement;
+  helpTooltip: Overlay;
+  measureTooltipElement: HTMLElement;
+  measureTooltip: Overlay;
+  continuePolygonMsg: string;
+  continueLineMsg: string;
+
+
+  typeSelect: HTMLSelectElement;
+  imgHtml: HTMLImageElement;
+  draw: Draw;
+
+
+
+  source: VectorSource;
+  vector: VectorLayer;
+
 
   constructor() { }
 
   ngAfterViewInit(): void {
     const divHtml: HTMLElement = this.divMap.nativeElement;
+    this.imgHtml = this.imageContainer.nativeElement;
+    this.typeSelect = this.selectElement.nativeElement;
 
-    // EPSG is en lng and lat respectly
-    // https://gis.stackexchange.com/questions/162825/what-is-the-type-of-epsg-of-longitude-latitude-not-point-co-ordinates
-    // const center = transform([-96.72365, 17.06542], 'EPSG:4326', 'EPSG:3857');
     const center = transform([-105.167, 27.667], 'EPSG:4326', 'EPSG:3857');
-
-
     // View
     const view = new View({
       center,
       zoom: 10,
-      maxZoom: 20,
       minZoom: 0,
-      projection: '32614'
-    });
-    // -------------------------------- Sources ------------------------------------------------------------------
-
-    const sourceImageWMS: ImageWMS =  new ImageWMS ({
-      url: 'http://187.189.192.102:8080/geoserver/wms',
-      params: {
-        LAYERS: 'topp:states',
-        VERSION: '1.3.0',
-        FORMAT: 'image/png',
-        TILED: 'true',
-        COLOR: '#ffffff'
-      }
     });
 
-    const sourceTileWMS =  new TileWMS({
-      url: 'http://187.189.192.102:8080/geoserver/GDB08011/wms',
-      params: {
-        LAYERS: 'GDB08011:p',
-        VERSION: '1.1.1',
-        FORMAT: 'image/png',
-        TILED: 'true',
-      },
-      serverType: 'geoserver',
-        // Countries have transparency, so do not fade tiles:
-        transition: 0,
-        tileGrid: createXYZ({ extent: [-13884991, 2870341, -7455066, 6338219] })
-        , projection: 'EPSG:900913'
-     });
-
-     // ---- bases layers -----
-    const sourceStamen = new Stamen({ layer: 'toner'  });
-
-    // -----vector sources ----
-    const sourceVector =  new VectorSource({
-      url: 'http://187.189.192.102:8080/geoserver/GDB08011/ows?' +
-      'service=WFS&' +
-      'version=1.1.0&' +
-      'request=GetFeature&' +
-      'typename=GDB08011:p&' +
-      'maxFeatures=1&' +
-      "&CQL_FILTER=cve_cat_ori='1005006006'&" +
-      'outputFormat=application/json',
-      format: new GeoJSON({featureProjection: 'EPSG:32613' }),
-      useSpatialIndex: false,
-    });
-
-
-
-    // -------------------------------- Layers -----------------------------------------------------------------
-    const simpleWMS = new ImageLayer({opacity: 0.6, source: sourceImageWMS });
-    simpleWMS.set('name', 'USA layer from Geoserver WMS demo');
-
-
-    const layerVms = new TileLayer({ source: sourceTileWMS });
-
-    const simpleOsm: Layer = new TileLayer({ source:
-      new OSM({ url: 'http://a.tile.stamen.com/terrain/{z}/{x}/{y}.png' }), });
-
-    const stamenLayer: Layer = new TileLayer({source: sourceStamen, });
-
-    // -------------------- Vector ---------------------------------------------------------------------------------
-
-    const _style = new Style({
-      fill: new Fill({
-        color: '#00FFF3',
-      }),
-      stroke: new Stroke({
-        color: '#00FFF3',
-        width: 3
-      }),
-    });
-
-    const vectorLayer = new VectorLayer({
-      source: sourceVector,
-      style: _style,
-    
-    });
-
-    // ------------------- End Vector ------------------------------------------------------------------------------
-
-
-    const basesLayers: LayerGroup = new LayerGroup({
-      layers: [simpleOsm, layerVms, vectorLayer]
-    });
-
-
-    // maps
-    simpleOsm.setVisible(true); //
-    stamenLayer.setVisible(false);
-
-    simpleWMS.setVisible(true); // countries top::usa
-
-    layerVms.setVisible(true);
-    vectorLayer.setVisible(true);
+    const simpleOsm: Layer = new TileLayer({ source: new OSM() });
 
     this.map = new Map({
-      layers: basesLayers,
+      layers: [simpleOsm,],
       target: divHtml,
       view
     });
 
 
-    const mapSize: Size = this.map.getSize();
-
-    // tslint:disable-next-line: only-arrow-functions
-    this.map.on('click' , function( event: MapBrowserEvent) {
-      // console.log(vectorLayer.getSource().getFeaturesCollection().getArray()[0].getGeometry().getExtent() );
-
-      const extent: Extent = vectorLayer.getSource().getFeaturesCollection().getArray()[0].getGeometry().getExtent();
-
-      // Try to zoom to multiple extent
-
-      const extentMultiple: Extent = createEmpty();
+    // Load first interaction
+    this.loadInteraction();
 
 
-      sourceVector.getFeaturesCollection().forEach(
-        (feature: Feature<Geometry>) =>  extend(extentMultiple, feature.getGeometry().getExtent()));
-
-
-      // const extentOfAllFeatures = vectorLayer.getSource().getExtent();
-
-      // console.log(this.map.getProperties());
-
-      // console.log( view.getProjection() );
-
-      console.log(vectorLayer);
-
-      transformExtent(extentMultiple, 'EPSG:4326', 'EPSG:3857');
-
-      // console.log( sourceVector.getFeaturesCollection().getArray()[0].getProperties() );
-
-      view.fit( extentMultiple ,{ maxZoom: 21 , size: mapSize} );
-
-    });
 
   }
+
+
+  // tslint:disable-next-line: align
+  loadInteraction(): void {
+    let continuePolygonMsg: string = 'Click to continue drawing the polygon';
+    let continueLineMsg: string = 'Click to continue drawing the line';
+
+    this.source = new VectorSource();
+
+    this.vector = new VectorLayer({
+      source: this.source,
+      style: new Style({
+        fill: new Fill({
+          color: 'rgba(255, 255, 255, 0.2)'
+        }),
+        stroke: new Stroke({
+          color: '#ffcc33',
+          width: 2
+        }),
+      })
+    });
+
+    this.map.on('pointermove', this.pointerMoveHandler);
+
+    this.createMeasureTooltip();
+    this.createHelpTooltip();
+
+    this.map.getViewport().addEventListener('mouseout', () => {
+      this.helpTooltipElement.classList.add('hidden');
+    });
+
+    this.typeSelect.onchange = () =>{
+      this.map.removeInteraction(this.draw);
+      this.addInteraction();
+    };
+
+    this.addInteraction();
+
+  }
+
+
+  addInteraction(): void {
+    const type: GeometryType = (this.typeSelect.value === 'area' ? GeometryType.POLYGON : GeometryType.LINE_STRING);
+
+    this.draw = new Draw({
+      source: this.source,
+      type,
+      style: new Style({
+        fill: new Fill({
+          color: 'rgba(255, 255, 255, 0.2)'
+        }),
+        stroke: new Stroke({
+          color: 'rgba(0, 0, 0, 0.5)',
+          lineDash: [10, 10],
+          width: 2
+        }),
+      })
+    });
+    this.map.addInteraction(this.draw);
+
+
+    let listener: EventsKey;
+    this.draw.on('drawstart',
+      (evt: DrawEvent) => {
+        // set sketch
+        this.sketch = evt.feature;
+
+        /** @type {import("../src/ol/coordinate.js").Coordinate|undefined} */
+        // let tooltipCoord = evt.getCoordinates();
+
+        listener = this.sketch.getGeometry().on('change', (evt: BaseEvent) => {
+          let geom = evt.target;
+          let output;
+          if (geom instanceof Polygon) {
+            output = this.formatArea(geom);
+
+            console.log(output);
+
+            // tooltipCoord = geom.getInteriorPoint().getCoordinates();
+          } else if (geom instanceof LineString) {
+            output = this.formatLength(geom);
+
+            console.log(output);
+
+            // tooltipCoord = geom.getLastCoordinate();
+          }
+          // this.measureTooltipElement.innerHTML = output;
+          //this.measureTooltip.setPosition(tooltipCoord);
+        });
+      });
+
+    this.draw.on('drawend',
+      () => {
+        this.measureTooltipElement.className = 'ol-tooltip ol-tooltip-static';
+        this.measureTooltip.setOffset([0, -7]);
+        // unset sketch
+        this.sketch = null;
+        // unset tooltip so that a new one can be created
+        this.measureTooltipElement = null;
+        this.createMeasureTooltip();
+        unByKey(listener);
+      });
+
+  }
+
+
+  pointerMoveHandler(evt: MapBrowserEvent): void {
+    if (evt.dragging) {
+      return;
+    }
+    let helpMsg = 'Click to start drawing';
+
+    if (this.sketch) {
+      const geom: Geometry = this.sketch.getGeometry();
+      if (geom instanceof Polygon) {
+        helpMsg = this.continuePolygonMsg;
+      } else if (geom instanceof LineString) {
+        helpMsg = this.continueLineMsg;
+      }
+    }
+
+
+    console.log(helpMsg);
+    //this.helpTooltipElement.innerHTML = helpMsg;
+    //this.helpTooltip.setPosition(evt.coordinate);
+    //this.helpTooltipElement.classList.remove('hidden');
+  }
+
+  /**
+   * Format length output.
+   * @param {LineString} line The line.
+   * @return {string} The formatted length.
+   */
+  formatLength(line: LineString): string {
+    let length: number = line.getLength();
+    let output: string;
+    if (length > 100) {
+      output = (Math.round(length / 1000 * 100) / 100) +
+        ' ' + 'km';
+    } else {
+      output = (Math.round(length * 100) / 100) +
+        ' ' + 'm';
+    }
+    return output;
+  }
+
+
+  /**
+   * Format area output.
+   * @param {Polygon} polygon The polygon.
+   * @return {string} Formatted area.
+   */
+  formatArea(polygon: Polygon): string {
+    let area: number = polygon.getArea();
+    let output: string;
+    if (area > 10000) {
+      output = (Math.round(area / 1000000 * 100) / 100) +
+        ' ' + 'km<sup>2</sup>';
+    } else {
+      output = (Math.round(area * 100) / 100) +
+        ' ' + 'm<sup>2</sup>';
+    }
+    return output;
+  }
+
+  /**
+   * Creates a new help tooltip
+   */
+  createHelpTooltip(): void {
+    if (this.helpTooltipElement) {
+      this.helpTooltipElement.parentNode.removeChild(this.helpTooltipElement);
+    }
+    this.helpTooltipElement = document.createElement('div');
+    this.helpTooltipElement.className = 'ol-tooltip hidden';
+    this.helpTooltip = new Overlay({
+      element: this.helpTooltipElement,
+      offset: [15, 0],
+      positioning: OverlayPositioning.CENTER_LEFT
+    });
+    this.map.addOverlay(this.helpTooltip);
+
+    console.log(this.helpTooltipElement);
+  }
+
+
+  /**
+   * Creates a new measure tooltip
+   */
+  createMeasureTooltip(): void {
+    if (this.measureTooltipElement) {
+      this.measureTooltipElement.parentNode.removeChild(this.measureTooltipElement);
+    }
+    this.measureTooltipElement = document.createElement('div');
+    this.measureTooltipElement.className = 'ol-tooltip ol-tooltip-measure';
+    this.measureTooltip = new Overlay({
+      element: this.measureTooltipElement,
+      offset: [0, -15],
+      positioning: OverlayPositioning.BOTTOM_CENTER
+    });
+    this.map.addOverlay(this.measureTooltip);
+  }
+
 
 }
